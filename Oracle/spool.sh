7ffa86bd56ccccdc1e0eed8cff9ab1acb877af5e
shaiknagurmeera868 <shaiknagurmeera868@gmail.com>
2025-10-17 02:58:27 +0530

    change

--- Oracle/spool.sh
+++ Oracle/spool.sh
@@ -1,5 +1,5 @@
 sqlplus "userfordatabase/password@databasename" <<EOF >/dev/null 2>&1
-spool /home/oracle/07022022-4.txt
+spool /pathforoutputfile/07022022-4.txt
 "past the name of sql files"
 spool off
 exit
