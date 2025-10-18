a57ddd75a9a57f12f261a69959ff320f39ee15f2
Nagur <97848001+shaiknagurmeera868@users.noreply.github.com>
2025-10-18 17:22:32 +0530

    Change

--- git/log.sh
+++ git/log.sh
@@ -3,7 +3,7 @@
 # Specify the branch name and output file path directly in the script
 output_file="path for output file"
 DFOrmat=$(date +%d%m%Y)
-outfile=${DFOrmat}
+outfile=${DFormat}
 
 # Path to Repo A
 repoA_path="Path for repository" # Specify your Repo A path here
@@ -13,9 +13,9 @@ cd "$repoA_path" || exit
 git checkout "Branch_name"
 
 # Get the logs with commit messages, file paths, and dates and save to the specified output file
-git log --name-only --pretty=format:"Commit: %h%nAuthor: %an%nDate: %ad%nMessage: %s%nFiles Changed:%n" > "$output_file/${outfile}"
+git log --name-only --pretty=format:"Commit: %H%nAuthor: %an%nDate: %ad%nMessage: %s%nFiles Changed:%n" > "$output_file/${outfile}"
 
 # Add a separator between commits in the log
-sed -i '/Files Changed:/a \--------------------------------------------\n' "$output_file/${outfile}"
+sed -i '/Files Changed:/a \\n--------------------------------------------\n' "$output_file/${outfile}"
 
 echo "Logs saved to $output_file/${outfile}"
