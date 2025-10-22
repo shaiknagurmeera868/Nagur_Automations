b2f3319329adbf416c1e9f10fc9a9e64cb59a79b
shaiknagurmeera868 <shaiknagurmeera868@gmail.com>
2025-10-22 16:27:04 +0530

    add

---
+++ Deployment/a.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+WLST_PATH="/Path/wlst.sh" # Path for wlst.sh
+USERNAME="username" # weblogic login user
+PASSWORD="Password" # login password
+ADMIN_SERVER_URL="url" # Replace with actual Admin Server URL
+
+WAR_FILE_PATH="/Path/NBC.war" # War file path
+APP_NAME="Name" # Name of Deployment
+MANAGED_SERVERS=("Managed_Server_11", "Managed_Server_12", "Managed_Server_21", "Managed_Server_22")
+TARGETS=$(IFS=, ; echo "${MANAGED_SERVERS[*]}")
+
+Deploy_SCRIPT="/tmp/deploy_script.py" # Creat a temporary python file
+
+cat <<EOF > $Deploy_SCRIPT
+# Connect to the Admin Server
+try:
+    connect('$USERNAME', '$PASSWORD', '$ADMIN_SERVER_URL')
+except:
+    print('Failed to connect to the Admin Server.')
+    exit()
+
+# Start an edit session
+edit()
+startEdit()
+
+# Deploy the application
+try:
+    deploy('$APP_NAME', '$WAR_FILE_PATH', targets='$TARGETS', stageMode='stage', upload='true')
+    save()
+    activate()
+except:
+    print('Deployment failed')
+    exit()
+
+# Start the target servers
+for server in ${MANAGED_SERVERS[@]}; do
+    try:
+        start(server, block='false')
+        print('Starting server: ' + server)
+    except:
+        print('Failed to start server: ' + server)
+	end
+done
+
+# Start the application
+try:
+    startApplication('$APP_NAME')
+except:
+    print('Failed to start application: ' + '$APP_NAME')
+
+# Disconnect from Admin Server
+disconnect()
+EOF
+
+$WLST_PATH $DEPLOY_SCRIPT
+rm $DEPLOY_SCRIPT
+
